78a3e62b71fc4000690165f0e2ad78bfa54df2ba
SallyMulupi <mulupisally2gmail.com>
2022-07-22 01:33:37 +0300

    add const for lastname

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -3,3 +3,4 @@ import DisplayData from './DisplayData';
 
 function ParentComponent() {
     const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Henry");
