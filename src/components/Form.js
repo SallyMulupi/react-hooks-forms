cb274e475a73c039c234f3db660c9265f9b084d8
SallyMulupi <mulupisally2gmail.com>
2022-07-22 01:31:23 +0300

    add import react

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,18 +5,20 @@ function Form({
   lastName,
   handleFirstNameChange,
   handleLastNameChange,
-}) 
-return (
-  <form>
-  <input type="text" onChange={handleFirstNameChange} value={firstName} />
-  <input type="text" onChange={handleLastNameChange} value={lastName} />
-   {/* <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
-   <input type="checkbox" id="newsletter" onChange={handleNewsletterChange} checked={newsletter}/> */}
-   nChange={handleNewsletterChange} checked={newsletter}/> */}
-   </form>
+}) {
+ 
+ 
+  
+  return (
+    <form>
+    <input type="text" onChange={handleFirstNameChange} value={firstName} />
+    <input type="text" onChange={handleLastNameChange} value={lastName} />
+    {/* <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+    <input type="checkbox" id="newsletter" onChange={handleNewsletterChange} checked={newsletter}/> */}
+    <button type="submit">Submit</button>
+  </form>
   );
-
+}
 
 export default Form;
-
     
