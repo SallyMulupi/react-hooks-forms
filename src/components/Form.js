ecb47df005959ef56ab8ae1f5389654f7bedc777
SallyMulupi <mulupisally2gmail.com>
2022-07-22 01:22:04 +0300

    add return

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,12 @@
 import React, { useState } from "react";
 
-function Form() {
-    firstName,
-    lastName,
-    handleFirstNameChange,
-    handleLastNameChange,
-  }) {
-    return ( <
-        form >
-        <
-        input type = "text"
-        value = { firstName }
-        /> <
-        input type = "text"
-        value = { lastName }
-        /> <
-        button type = "submit" > Submit < /button> < /
-        form >
-    );
-}
-
-export default Form;
+function Form({
+  firstName,
+  lastName,
+  handleFirstNameChange,
+  handleLastNameChange,
+}) 
+return (
+  
+)
+    
